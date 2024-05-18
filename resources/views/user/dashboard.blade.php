c80e0b248c1cb695424eea8882626ca348178411
zeyad_sofy <75764257+zeyadsofy@users.noreply.github.com>
2024-05-18 14:55:55 +0300

    second commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -89,7 +89,7 @@
                 document.write(new Date().getFullYear())
               </script>2024,
               made with <i class="fa fa-heart" aria-hidden="true"></i> by
-              <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+              <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">S O F Y</a>
               for a better web.
             </div>
           </div>
@@ -99,13 +99,7 @@
                 <a href="{{ route("service1") }}" class="nav-link text-muted" >Service</a>
               </li>
               <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted">Your Requests</a>
               </li>
             </ul>
           </div>
